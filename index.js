4a465fa6439b95a82f9cea16bcc6f17c7a95bf0b
Mahdi Mostafavi <mmostafavi247@gamil.com>
2021-05-12 14:34:27 +0430

    Add database connection

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// entry point
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+ /*
+  you may need to add authentication here
+    app.use(student_Auth)
+    app.use(instructor_Auth)
+  you may need to add authentication here
+ */
+
+
+mongoose.set('returnOriginal', false);
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_USER_PASSWORD}@cluster0.oygyj.mongodb.net/${process.env.DATABASE_DEFAULT_DATABASE}?retryWrites=true&w=majority`,
+		{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+	)
+	.then(() => {
+    app.listen(process.env.PORT)
+		console.log(`app is listening in port: ${process.env.PORT}`);
+	})
+	.catch((err) => {
+		throw err;
+	});
+
+
